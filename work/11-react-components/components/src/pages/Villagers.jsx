54a53f393c8e14886ef49f41856d88083b3c6f7d
OK_Kehan <tang.keh@northeastern.edu>
2023-11-29 15:34:21 -0800

    almost finish: modify the dialog part

--- work/11-react-components/components/src/pages/Villagers.jsx
+++ work/11-react-components/components/src/pages/Villagers.jsx
@@ -6,88 +6,86 @@ import Audie from '../assets/Audie_NH.png';
 import Beau from '../assets/Beau_NH.png';
 import Katt from '../assets/Katt_NH.png';
 import Marshal from '../assets/Marshal_NH.png';
+import Card from '../components/card';
+import Dialog from '../components/Dialog';
+import { useState } from 'react';
+
 
 const Villagers = () => {
+  // here is to let the whole page only shows one dialog
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const toggleDialog = () => {
+      setIsDialogOpen(true);
+
+    }
+
+  
+    const closeDialog = () => {
+      setIsDialogOpen(false);
+    };
+
+    const animal_data = [
+        {
+        name: 'Antonio',
+        imageSrc: Antonio,
+        species: 'Anteater ',
+        personality: 'Jock',
+        gender: 'Male',
+      },
+      {
+        name: 'Marshal',
+        imageSrc: Marshal,
+        species: 'Squirrel ',
+        personality: 'Smug',
+        gender: 'Male',
+      },
+      {
+        name: 'Katt',
+        imageSrc: Katt,
+        species: 'Cat ',
+        personality: 'Big Sister',
+        gender: 'Female',
+      },
+      {
+        name: 'Ankha',
+        imageSrc: Ankha,
+        species: 'Cat',
+        personality: 'Snooty',
+        gender: 'Female',
+      },
+      {
+        name: 'Audie',
+        imageSrc: Audie,
+        species: 'Wolf',
+        personality: 'Peppy',
+        gender: 'Female',
+      },
+      {
+        name: 'Beau',
+        imageSrc: Beau,
+        species: 'Beau ',
+        personality: 'Lazy',
+        gender: 'Male',
+      }
+    ];
+
     return (
         <div className = 'cardset'>
-            <div className = "card">
-                <h2 className = "card_title">Antonio</h2>
-                <img className = 'photo' src={Antonio} alt="Antonio's portrait" />
-                <div className = 'card_content'>
-                    <p> Species: Anteater  <br />
-                    	Personality: Jock  <br />
-                        Gender: Male  <br />
-                        Birthday: October 20th  <br />
-                        Favorite saying	"Always go for the burn!"
-                    </p>
-                    <p>Phasellus non risus <span className="subscribe-text">Subscribe</span>  at purus vulputate consequat.</p>
-                </div>
-            </div>
-            <div className = "card">
-                <h2 className = "card_title">Marshal</h2>
-                <img className = 'photo' src={Marshal}  alt="Marshal's portrait" />
-                <div className = 'card_content'>
-                    <p> Species: Squirrel  <br />
-                    	Personality: Smug  <br />
-                        Gender: Male  <br />
-                        Birthday: September 29th  <br />
-                        Favorite saying	"Seize the day."
-                    </p>
-                    <p>Phasellus non risus <span className="subscribe-text">Subscribe</span>  at purus vulputate consequat.</p>
-                </div>
-            </div>
-            <div className = "card">
-                <h2 className = "card_title">Katt</h2>
-                <img className = 'photo' src={Katt}  alt="Katt's portrait" />
-                <div className = 'card_content'>
-                    <p> Species: Cat  <br />
-                    	Personality: Big sister  <br />
-                        Gender: Female  <br />
-                        Birthday: April 27th  <br />
-                        Favorite saying	"MeowMEOWmeow!"
-                    </p>
-                    <p>Phasellus non risus <span className="subscribe-text">Subscribe</span>  at purus vulputate consequat.</p>
-                </div>
-            </div> 
-            <div className = "card">
-                <h2 className = "card_title">Ankha</h2>
-                <img className = 'photo' src = {Ankha} alt="Ankha's portrait" />
-                <div className = 'card_content'>
-                    <p> Species: Cat <br />
-                    	Personality: Snooty  <br />
-                        Gender: Female  <br />
-                        Birthday: September 22nd  <br />
-                        Favorite saying	"All that glitters is not gold."
-                    </p>
-                    <p>Phasellus non risus <span className="subscribe-text">Subscribe</span>  at purus vulputate consequat.</p>
-                </div>
-            </div>
-            <div className = "card">
-                <h2 className = "card_title">Audie</h2>
-                <img className = 'photo' src={Audie}  alt="Audie's portrait" />
-                <div className = 'card_content'>
-                    <p> <br />Species: Wolf  <br />
-                    	Personality: Peppy  <br />
-                        Gender: Female  <br />
-                        Birthday: August 31st  <br />
-                        Favorite saying	"Be the kind of person, <br /> your future self won't regret having been."
-                    </p>
-                    <p>Phasellus non risus <span className="subscribe-text">Subscribe</span>  at purus vulputate consequat.</p>
-                </div>
-            </div>
-            <div className = "card">
-                <h2 className = "card_title">Beau</h2>
-                <img className = 'photo' src={Beau}  alt="Beau's portrait" />
-                <div className = 'card_content'>
-                    <p> Species: Deer  <br />
-                    	Personality: Lazy  <br />
-                        Gender: Male  <br />
-                        Birthday: April 5th  <br />
-                        Favorite saying	"You snooze, you lose."
-                    </p>
-                    <p>Phasellus non risus <span className="subscribe-text">Subscribe</span>  at purus vulputate consequat.</p>
-                </div>
-            </div>
+             {animal_data.map((animal, index) => (
+                <Card
+                    key = {index}
+                    name = {animal.name}
+                    imageSrc= {animal.imageSrc}
+                    species= {animal.species}
+                    personality= {animal.personality}
+                    gender= {animal.gender}
+                    toggleDialog={toggleDialog}
+                />
+                
+              ))};
+               {isDialogOpen && <Dialog onClose={closeDialog} />}
+
         </div>
                 
     );
